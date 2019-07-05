500079c1b7240e0018f659ca0c7314caada7fcd8
Olivia Ruiz-Knott <livvy@noteflight.com>
2019-07-04 22:25:28 -0400

    Starts some structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+
+import BoardSection from './BoardSection';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="sidebar">This will be the Messages Bar.</div>
+      <BoardSection />
+      <div className="sidebar">This will be the Stats Bar.</div>
     </div>
   );
 }
---
+++ src/BoardSection.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './BoardSection.scss';
+
+class BoardSection extends Component {
+  render() {
+    return (
+      <div className="BoardSection">This will be where the Boards live.</div>
+    )
+  }
+}
+
+export default BoardSection;
