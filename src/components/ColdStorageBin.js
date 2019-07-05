88e053892fa38362a6cacd01b60a468033361fd6
Olivia Ruiz-Knott <livvy@noteflight.com>
2019-07-05 18:48:51 -0400

    Some styling

--- src/components/ColdStorageBin.js
+++ src/components/ColdStorageBin.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import '../styles/ColdStorageBin.scss';
 
 class ColdStorageBin extends Component {
   render() {
+    let style = { "backgroundColor": this.props.color }
     return (
       <div className="ColdStorageBin">
-        <p>{this.props.id} {this.props.stored}/{this.props.limit} <button>+</button></p>
+        <div className={`dnaName ${this.props.shape}`} style={style}>{this.props.id}</div>
+        <p className="amounts">{this.props.stored} / {this.props.limit}</p>
+        <button>+</button>
       </div>
     )
   }
