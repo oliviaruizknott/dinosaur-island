f688c53325e34419ab89bcc2714f3a63203b7d60
Olivia Ruiz-Knott <livvy@noteflight.com>
2019-08-21 18:29:13 -0400

    Handle click (fake)

--- src/components/ColdStorageBin.js
+++ src/components/ColdStorageBin.js
@@ -2,13 +2,23 @@ import React, { Component } from 'react';
 import '../styles/ColdStorageBin.scss';
 
 class ColdStorageBin extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    console.log(`We’ll increase cold storage for ${e.target.id}`)
+  }
+
   render() {
     let style = { "backgroundColor": this.props.color }
     return (
       <div className="ColdStorageBin">
         <div className={`dnaName ${this.props.shape}`} style={style}>{this.props.id}</div>
         <p className="amounts">{this.props.stored} / {this.props.limit}</p>
-        <button>+</button>
+        <button id={this.props.id} onClick={this.handleClick}>+</button>
       </div>
     )
   }
