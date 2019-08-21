274b97bff91b4cc014d85e45a87cc1dcb1d59aae
Olivia Ruiz-Knott <livvy@noteflight.com>
2019-08-21 18:35:47 -0400

    Miscellaneous fixes

--- src/utilities.js
+++ src/utilities.js
@@ -3,7 +3,6 @@ export function shuffle(array) {
 
   // While there remain elements to shuffle...
   while (0 !== currentIndex) {
-
     // Pick a remaining element...
     randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex -= 1;
